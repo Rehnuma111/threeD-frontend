5ae67ed9b0164cf3b8bc130aa80894561e359bb8
Rehnuma Bano <70592871+Rehnuma111@users.noreply.github.com>
2023-03-11 00:37:32 +0530

    backend url added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const app_config = {
-    api_url: "http://localhost:5000"
+    // api_url: "http://localhost:5000",
+    api_url: "https://threed-backend.onrender.com"
+
 };
 
 export default app_config;
